4563d405c9047a958aa045e6965b2079b24de5e8
Xavier MALPARTY <xavier@malparty.fr>
2021-01-18 20:14:35 +0700

    update fix

--- deploy.js
+++ deploy.js
@@ -22,6 +22,7 @@ sftp.connect({
     port: process.env.FTP_DEPLOY_PORT,
     username: process.env.FTP_DEPLOY_USERNAME,
     password: process.env.FTP_DEPLOY_PASSWORD,
+    debug:true
 })
     .then(() => scanLocalFiles())
     .then(items => {
